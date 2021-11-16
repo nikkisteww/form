f54849830d1386c6a68d71e4cf496db1b9a00ee0
nikkisteww <89221253+nikkisteww@users.noreply.github.com>
2021-11-16 14:24:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,34 @@ $("#button").click(function() {
       }
  
   
+   $('#btnLoadData').click(function() {
+    console.log("clicked");
+
+
+    let jsonURL = "https://barrycumbie.github.io/376-india-lab/demo.json";
+
+    $.ajax({
+        url: jsonURL,
+        dataType: "json",
+        success: function(result) {
+            //data = $.parseJSON(result);
+            console.log(result);
+            $.each(result, function(result) {
+                $("#dataContainer").append(result + " ");
+            });
+        }
+    });
+
+    // $.ajax({
+    //     url: jsonURL,
+    //     jsonp: "callback",
+    //     dataType: "jsonp",
+    //     data: { format: "json" },
+    //     success: function(response) { console.log(response); }
+    // });
+});
+
+
+
+// end of doc ready f/n
+// });
