49fc9e0d9d57918c5ebc013ac9e647e339aa790d
nikkisteww <89221253+nikkisteww@users.noreply.github.com>
2021-11-24 13:28:55 -0600

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -4,17 +4,16 @@
 $('#btnLoadData').click(function() {
     console.log("clicked");
 
-    //let jsonURL = "https://www.w3schools.com/jquery/demo_ajax_json.js";
+    
 
     let jsonURL = "demo.json";
 
-    // let jsonURL = "https://barrycumbie.github.io/376-india-lab/demo.json";
-
+   
     $.ajax({
         url: jsonURL,
         dataType: "json",
         success: function(data) {
-            //can log either the entire data or invoke specific properties data.zipCode...
+            
             console.log(data.firstName);
             
             //loads first name into my first input box (in example #1)
@@ -34,9 +33,7 @@ $('#btnLoadData').click(function() {
             $.each(data, function (key, val) {
                 //step through results
                 console.log(key, val);
-                //get each data item
-                //put it in the right place
-                //$("#dataContainer").append(key + " " + val);
+               
                 $(`#${key}`).val(val);
                 
             });
